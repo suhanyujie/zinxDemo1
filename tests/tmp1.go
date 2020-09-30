b7adec3d347809386dad32c0ff04c3a4a84864d8
suhanyujie <suhanyujie@qq.com>
2020-09-30 17:15:25 +0800

    take some test

--- tests/tmp1.go
+++ tests/tmp1.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 func main() {
 	// map 内字段的匹配
@@ -9,7 +13,16 @@ func main() {
 	// 从 map 中删除一个元素
 	// deleteEle()
 	// 校验结构体是否实现了某个接口
-	doTest1()
+	// doTest1()
+	//efunc()
+	// test 引用传参
+	// refFunc1()
+	// 函数 len
+	// testFuncLen()
+	// 测试 switch
+	//testSwitch()
+	// 切片和切片指针
+	sliceAndSlicePoint()
 }
 
 func IsLocalForum(needleName string) map[string]interface{} {
@@ -74,3 +87,79 @@ func doTest1() {
 }
 
 // 校验结构体是否实现了某个接口 end
+
+// 返回的异常类型是指针，实际返回是 nil start
+type eP struct {
+}
+
+func (*eP) Error() string {
+	return "err"
+}
+
+var (
+	Err1 = errors.New("test error")
+)
+
+func efunc1() *eP {
+	return nil
+}
+
+func efunc2() error {
+	return efunc1()
+	//var err error = efunc1()
+	//err := efunc1()
+	//log.Printf("%v, %v\n", err, err == nil)
+	//return nil
+	// err1 := err.(*eP)
+	// log.Printf("%v,%v\n", err1, err1 == nil)
+}
+
+func efunc() error {
+	v1 := efunc2()
+	log.Printf("%v\n", v1)
+	return nil
+}
+
+// 返回的异常类型是指针，实际返回是 nil end
+
+// 引用传参 start
+func refFunc1() {
+	s1 := "苏汉"
+	b1 := []byte(s1)
+	log.Printf("%v\n", b1)
+	refFunc2(&b1)
+	log.Printf("%v\n", b1)
+}
+
+func refFunc2(b1 *[]byte) {
+	*b1 = append(*b1, 10)
+	*b1 = []byte("fafds")
+}
+
+// 函数 len
+func testFuncLen() {
+	s1 := "test1123123"
+	log.Printf("string len is: %d\n", len(s1))
+}
+
+//test switch
+func testSwitch() {
+	t1 := 4
+	switch t1 {
+	case 1:
+		log.Printf("1 val is: %v\n", t1)
+	case 2:
+		log.Printf("2 val is: %v\n", t1)
+	case 3, 4:
+		log.Printf("3 val is: %v\n", t1)
+	}
+}
+
+// 切片和切片指针
+func sliceAndSlicePoint() {
+	s1 := []string{"hello", "world"}
+	ps1 := []*[]string{}
+	ps1 = append(ps1, &s1)
+
+	log.Printf("%v\n", ps1)
+}
