15cac933b9e7b2b1dce89ba3379d93e2a564530a
suhanyujie <suhanyujie@qq.com>
2022-03-21 09:45:53 +0800

    add one test for json

--- tests/2022/t1/t1_test.go
+++ tests/2022/t1/t1_test.go
@@ -1,7 +1,20 @@
 package t1
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Stu1 struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
 func TestJson1(t *testing.T) {
-
+	json1 := "{\"name\": \"Samuel\", \"age\": 21}"
+	stu1 := Stu1{}
+	// 只有导出的 field 才能进行反序列化
+	json.Unmarshal([]byte(json1), &stu1)
+	fmt.Printf("%+v", stu1)
 }
