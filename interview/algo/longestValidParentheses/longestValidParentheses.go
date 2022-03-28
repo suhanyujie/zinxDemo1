8a2b423394a9a68d949b1b152ddb1ed20cb0ee8b
suhanyujie <suhanyujie@qq.com>
2022-03-28 10:00:00 +0800

    add one problem

---
+++ interview/algo/longestValidParentheses/longestValidParentheses.go
@@ -0,0 +1,46 @@
+package longestValidParentheses
+
+/**
+题目名称：32. 最长有效括号
+题目地址：https://leetcode-cn.com/problems/longest-valid-parentheses/
+
+## 题目描述
+给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+
+提示：
+* `0 <= s.length <= 3 * 104`
+* `s[i]` 为 '(' 或 ')'
+
+### 示例 1
+
+```
+输入：s = "(()"
+输出：2
+解释：最长有效括号子串是 "()"
+```
+
+### 示例 2
+
+```
+输入：s = ")()())"
+输出：4
+解释：最长有效括号子串是 "()()"
+```
+
+## 思路分析
+
+## 总结
+
+### 复杂度分析
+
+
+
+## ref
+* https://leetcode-cn.com/problems/longest-valid-parentheses/
+
+*/
+
+func longestValidParentheses(s string) int {
+
+	return 0
+}
