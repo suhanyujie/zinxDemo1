962642bb1de82ff757e219eb43b5c7af4814ac8d
suhanyujie <suhanyujie@qq.com>
2022-03-29 12:19:33 +0800

    add some note

--- tests/2022/t1/t1_test.go
+++ tests/2022/t1/t1_test.go
@@ -3,12 +3,14 @@ package t1
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 type Stu1 struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Name  string            `json:"name"`
+	Age   int               `json:"age"`
+	Tools map[string]string `json:"tools"`
 }
 
 func TestJson1(t *testing.T) {
@@ -17,4 +19,19 @@ func TestJson1(t *testing.T) {
 	// 只有导出的 field 才能进行反序列化
 	json.Unmarshal([]byte(json1), &stu1)
 	fmt.Printf("%+v", stu1)
+}
+
+// 结构体能否判等。可以，但如果结构体里有复合类型，则不行
+func TestEqual1(t *testing.T) {
+	stu1 := Stu1{
+		Name: "1",
+	}
+	stu2 := Stu1{
+		Name: "1",
+	}
+	if reflect.DeepEqual(stu1, stu2) {
+		t.Log("euqal")
+		return
+	}
+	t.Log("--end--")
 }
